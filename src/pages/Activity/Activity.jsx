a13be7f281eb8a3f4e9219c06867f5e00055e90e
hemanth0504 <hemanth.teegala5@gmail.com>
2025-09-07 13:02:31 -0400

    initial frontend commit

---
+++ src/pages/Activity/Activity.jsx
@@ -0,0 +1,12 @@
+import tradingHistory from '../Portfolio/tradingHistory'
+
+const Activity = () => {
+  return (
+    <div className='px-20'>
+      <p className='py-5 pb-10 text-2xl font-semibold'>trading History</p>
+        <tradingHistory/>
+    </div>
+  )
+}
+
+export default Activity
