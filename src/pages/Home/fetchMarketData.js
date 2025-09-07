2b6c0210f1d7ae0270070794dfca7f1248fa308e
hemanth0504 <hemanth.teegala5@gmail.com>
2025-09-07 13:24:35 -0400

    api key fix

--- src/pages/Home/fetchMarketData.js
+++ src/pages/Home/fetchMarketData.js
@@ -1,25 +1,25 @@
-import axios from 'axios';
-export const dataType="Time Series (Daily)"
-const fetchData = async (keyword,symbol) => {
+import axios from "axios";
+
+export const dataType = "Time Series (Daily)";
+
+const fetchData = async (keyword, symbol) => {
   try {
-    const response = await axios.get('https://www.alphavantage.co/query', {
+    const response = await axios.get("https://www.alphavantage.co/query", {
       params: {
         function: keyword,
         symbol: symbol,
-        apikey: 'my api keyd', // Replace 'demo' with your actual API key
-        market:"EUR"
-      }
+        apikey: import.meta.env.VITE_ALPHAVANTAGE_API_KEY,
+      },
     });
 
-    // Check if data was retrieved successfully
     if (response.status === 200) {
-      return response.data; // Return the data
+      return response.data;
     } else {
-      throw new Error('Failed to fetch data');
+      throw new Error("Failed to fetch data");
     }
   } catch (error) {
-    console.error('Error fetching data:', error);
-    return null; // Return null if there's an error
+    console.error("Error fetching data:", error);
+    return null;
   }
 };
 
